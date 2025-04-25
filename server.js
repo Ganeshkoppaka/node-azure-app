0a88d6163df934eba3e528785d4304ab23fa722d
Ganeshkoppaka <102478806+Ganeshkoppaka@users.noreply.github.com>
2025-04-25 10:40:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello from Azure App Service 🚀</h1>');
+  res.end('<h1>Hello from Azure App Service, your github actions are working properly 🚀</h1>');
 });
 
 server.listen(port, hostname, () => {
